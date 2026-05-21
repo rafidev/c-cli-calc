009228d71c975fadca7618402c0a1e3ae5f41e63
rafilaos <rafisdevelopment@gmail.com>
2026-05-21 09:21:21 +0200

    feat: add Input numbers

--- main.c
+++ main.c
@@ -17,8 +17,14 @@ float div(float num1, float num2) {
 }
 
 int main() {
-    float num1 = 4.5;
-    float num2 = 6.3;
+    float num1 = 0;
+    float num2 = 0;
+
+    printf("Input num1: ");
+    scanf("%f", &num1);
+
+    printf("Input num2: ");
+    scanf("%f", &num2);
 
     float sum = add(num1, num2);
     printf("%g + %g = %g\n", num1, num2, sum);
